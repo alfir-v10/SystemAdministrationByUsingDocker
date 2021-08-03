d78194d481f173d6001e866cd41f81b61dc64181
alfir-v10 <alfir.v10@gmail.com>
2021-08-03 18:44:09 +0400

    title add

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -8,7 +8,7 @@ if ! [ -f "/var/www/html/wp-config.php" ]; then
 	wp core download --locale=ru_RU --path=wp --allow-root 
 	cd wp	
     wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER_NAME} --dbpass=${DB_USER_PASSWORD} --allow-root
-    wp core install --url=${URL} --admin_user=${ADM_NAME} --admin_password=${ADM_PASSWORD} --admin_email=${ADM_EMAIL} --skip-email --allow-root
+    wp core install --url=${URL} --admin_user=${ADM_NAME} --title="mysite" --admin_password=${ADM_PASSWORD} --admin_email=${ADM_EMAIL} --skip-email --allow-root
     wp user create --allow-root vaska vaskapupkin@mail.ru --role=author --user_pass=vaska
     mv * /var/www/html/
 fi
