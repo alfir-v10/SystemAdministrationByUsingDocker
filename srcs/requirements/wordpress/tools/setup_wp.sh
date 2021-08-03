20fff22eadc93c04a5dd311f76bf39eca4dbf0ae
alfir-v10 <alfir.v10@gmail.com>
2021-08-03 17:12:33 +0400

    joke

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -7,9 +7,9 @@ if ! [ -f "/var/www/html/wp-config.php" ]; then
     mv wp-cli.phar /usr/local/bin/wp
 	wp core download --locale=ru_RU --path=wp --allow-root 
 	cd wp	
-    wp core config --dbhost=${DB_HOST} --dbname=${MYSQL_WP_NAME} --dbuser=${DB_USER_NAME} --dbpass=${DB_USER_PASSWORD} --allow-root
+    wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER_NAME} --dbpass=${DB_USER_PASSWORD} --allow-root
     wp core install --url=${URL} --admin_user=${ADM_NAME} --admin_password=${ADM_PASSWORD} --admin_email=${ADM_EMAIL} --skip-email --allow-root
-    wp user create --allow-root vaska vaskapupkin@mail.ru --role=author --user_pass=btwee
+    wp user create --allow-root vaska vaskapupkin@mail.ru --role=author --user_pass=vaska
     mv * /var/www/html/
 fi
 
